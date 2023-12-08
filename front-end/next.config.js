1a45b5da5ff7bcdbf38c809c7c3aa53d80c8a1b9
unknown <kusokpocki@gmail.com>
2023-12-07 19:53:45 -0700

    back-end port

--- front-end/next.config.js
+++ front-end/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    SERVER: "http://localhost:3000/",
+    SERVER: "http://localhost:3002/",
     SPACE: "gu8txoix5hr4",
     ACCESSE_TOKEN: "ozTPXf0yRbbEAPKiFpjxBX1tJq5O1GL5I8jvozETBPU",
     REVALIDATE: "3600"
